ee29045f812527c8c0c4e1616aa7ddcc39b0fb6c
guy@moment.me <guysopher@gmail.com>
2014-10-09 21:36:28 +0300

    add latlon to kennel

--- models/kennel.js
+++ models/kennel.js
@@ -9,6 +9,7 @@ var kennelSchema = new Schema({
     name     : { type: String, default:'Untitled Kennel', required: true },
     username : { type: String, default:'username1,username2,username3', required: true },
     info     : { type: String, default:' ', required: false },
+    latlon     : { type: String, default:' ', required: false },
     address  : { type: String, default:' ', required: false }
 });
 kennelSchema.plugin(timestamps);
