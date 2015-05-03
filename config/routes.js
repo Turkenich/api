97a65729162eda00cb163c134d7739ca9647128e
Guy Sopher <guysopher@gmail.com>
2015-05-03 16:24:23 +0300

    add ping route

--- config/routes.js
+++ config/routes.js
@@ -18,6 +18,7 @@ module.exports = function(app) {
     app.options('*', cors());
 
     // services
+    app.get('/', services.ping);
     app.get('/ping', services.ping);
 
 
@@ -102,56 +103,4 @@ module.exports = function(app) {
         app.put('/:id', prices.update);
         app.del('/:id', prices.delete);
     });
-
-    /*
-
-    //users
-    app.namespace('/user', function() {
-        app.get('/', users.list);
-        app.get('/:id', users.get);
-        app.post('/', users.create);
-        app.put('/:id', users.update);
-        app.del('/:id', users.delete);
-    });
-
-    //media
-    app.namespace('/media', function() {
-        app.get('/', media.list);
-        app.get('/last', media.last);
-        app.get('/:id', media.get);
-        app.post('/', media.create);
-        app.put('/:id', media.update);
-        app.del('/:id', media.delete);
-    });
-
-    //kennels
-    app.namespace('/kennel', function() {
-        app.get('/', kennels.list);
-        app.get('/:id', kennels.get);
-        app.post('/', kennels.create);
-        app.put('/:id', kennels.update);
-        app.del('/:id', kennels.delete);
-    });
-
-    //treats
-    app.namespace('/treat', function() {
-        app.get('/', treats.list);
-        app.get('/:id', treats.get);
-        app.post('/', treats.create);
-        app.put('/:id', treats.update);
-        app.del('/:id', treats.delete);
-    });
-
-    //donations
-    app.namespace('/donation', function() {
-        app.get('/', donations.list);
-        app.get('/given', donations.given);
-        app.get('/pending', donations.pending);
-        app.get('/:id', donations.get);
-        app.post('/', donations.create);
-        app.post('/approve', donations.approve);
-        app.put('/:id', donations.update);
-        app.del('/:id', donations.delete);
-    });
-*/
 };
